b3ca89575fc7d05c2fb6df3125e4bb9fb6302079
Ebele Okemba <okemba50@gmail.com>
2023-02-24 19:34:49 +0000

    create a function that ask the user the memeber of the team they wished to add next or exist the build team

--- index.js
+++ index.js
@@ -48,6 +48,41 @@ const buildTeam = () =>{
             nextStep();
         })
     }
+
+    // function that ask the user the memeber of the team they wished to add next or exist the build team
+    const nextStep = () => {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'options',
+                message: "Which option do you what to perform",
+                choices: ['Add an engineer', 'Add a manager','Add an intern', 'Finish building the team'],
+            },
+    
+        ]).then((answers) => {
+            if (answers.options === 'Add an engineer') {
+                console.log("You which to add an Engineer");
+                addEngineer();
+            }
+            else if (answers.options === 'Add a manager') {
+                console.log("You which to add a Manager");
+                addManager();
+            }
+            else if (answers.options === 'Add an intern') {
+                console.log("You which to add an Intern");
+                addIntern();
+            }
+            else if (answers.options === 'Finish building the team') {
+                console.log("You are done");
+                exitApp();
+    
+            }
+            else {
+                console.log("Nothing selected")
+                nextStep();
+            }
+        });
+    }
     
 }
 buildTeam()
